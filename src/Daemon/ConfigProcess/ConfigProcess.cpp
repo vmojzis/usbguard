08b8361b15612df3ce3790bd6bb778bccc8981b1
Daniel Kopecek <dkopecek@redhat.com>
2015-08-16 19:18:36 +0200

    Implemented the config process

--- src/Daemon/ConfigProcess/ConfigProcess.cpp
+++ src/Daemon/ConfigProcess/ConfigProcess.cpp
@@ -1,9 +1,25 @@
 #include "ConfigProcess.hpp"
+#include "Common/JSON.hpp"
 #include <stdlib.h>
 
 namespace usbguard
 {
+  /*
+   * Recognized configuration option names. If an
+   * unknown setting is found in the config file,
+   * a warning message will be displayed.
+   */
+  const StringVector G_config_known_names = {
+    "RuleFile",
+    "ImplicitPolicyTarget",
+    "PresentDevicePolicy",
+    "PresentControllerPolicy",
+    "IPCAllowedUsers",
+    "IPCAllowedGroups"
+  };
+
   ConfigProcess::ConfigProcess()
+    : _config(G_config_known_names)
   {
   }
 
@@ -13,6 +29,55 @@ namespace usbguard
 
   int ConfigProcess::main(int argc, char *argv[])
   {
-    return EXIT_SUCCESS;
+    int ret = EXIT_FAILURE;
+
+    _config.open(USBGUARD_CONFIG_PATH);
+
+    while(true) {
+      if (messageBusWait() != 1) {
+        continue;
+      }
+
+      std::string json_string;
+      const pid_t peer_pid = messageBusRecv(-1, json_string);
+
+      processMessage(peer_pid, json_string);
+    }
+
+    _config.close();
+
+    return ret;
   }
+
+  void ConfigProcess::processMessage(const pid_t peer_pid, const std::string& json_string)
+  {
+    const json message = json_string;
+
+    if (message["op"] == "get") {
+      const std::string key = message["key"];
+      processConfigGet(peer_pid, key);
+    }
+    else if (message["op"] == "set") {
+      const std::string key = message["key"];
+      const std::string val = message["val"];
+      processConfigSet(peer_pid, key, val);
+    }
+
+    return;
+  }
+
+  void ConfigProcess::processConfigGet(const pid_t peer_pid, const std::string& key)
+  {
+    const std::string& val = _config.getSettingValue(key);
+    messageBusSend(peer_pid, val);
+    return;
+  }
+
+  void ConfigProcess::processConfigSet(const pid_t peer_pid, const std::string& key,
+      const std::string& val)
+  {
+    _config.setSettingValue(key, val);
+    return;
+  }
+
 } /* namespace usbguard */
--- src/Daemon/ConfigProcess/ConfigProcess.hpp
+++ src/Daemon/ConfigProcess/ConfigProcess.hpp
@@ -1,16 +1,28 @@
 #pragma once
 #include <pgl.hpp>
+#include "ConfigFile.hpp"
+
+#ifndef USBGUARD_CONFIG_PATH
+#define USBGUARD_CONFIG_PATH "/etc/usbguard/usbguard-daemon.conf"
+#endif
 
 namespace usbguard
 {
   class ConfigProcess : public pgl::Process
   {
     public:
-    ConfigProcess();
-    ~ConfigProcess();
+      ConfigProcess();
+      ~ConfigProcess();
 
-    int main(int argc, char *argv[]);
+      int main(int argc, char *argv[]);
+
+    protected:
+      void processMessage(const pid_t peer_pid, const std::string& json_string);
+      void processConfigGet(const pid_t peer_pid, const std::string& key);
+      void processConfigSet(const pid_t peer_pid, const std::string& key, const std::string& val);
 
     private:
+      ConfigFile _config;
   };
 } /* namespace usbguard */
+
